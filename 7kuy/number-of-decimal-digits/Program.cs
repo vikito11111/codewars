82135b6255f65373689ecacebee807f3bd58f7a6
vikito11111 <vikito11111@gmail.com>
2023-09-29 16:22:56 +0300

    Initial commit

---
+++ 7kuy/number-of-decimal-digits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var result = Digits(999);
+
+        Console.WriteLine(result);
+    }
+
+    public static int Digits(ulong n)
+    {
+        return n.ToString().Length;
+    }
+}
