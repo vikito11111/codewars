6021df1ed4b0bba87ce04110bf9ded741f7caa8b
vikito11111 <vikito11111@gmail.com>
2023-09-29 16:13:11 +0300

    Initial commit

---
+++ 8kuy/what-is-between/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var result = Between(1, 5);
+
+        foreach (var num in result)
+        {
+            sb.Append(num + " ");
+        }
+
+        Console.WriteLine(sb.ToString().TrimEnd());
+    }
+
+    public static int[] Between(int a, int b)
+    {
+        List<int> lst = new List<int>();
+
+        for (int i = a; i <= b; i++)
+        {
+            lst.Add(i);
+        }
+
+        return lst.ToArray();
+    }
+}
